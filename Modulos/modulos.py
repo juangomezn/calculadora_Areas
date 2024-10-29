b5b3f26a7adb710a5e0a693724a6cb2e6c282ac9
juangomezn <gomezninoj681@gmail.com>
2024-10-28 19:40:24 -0500

    feat :rocket: Implementacion del menu de usuario

---
+++ Modulos/modulos.py
@@ -0,0 +1,7 @@
+def menu():
+    print("Calculadora de Areas")
+    print("""   1. Area de un rectangulo
+                2. Area de un circulo
+                3. Area de un triangulo
+                4. Area de un rombo
+                0. Salir""")
---
+++ main.py
@@ -0,0 +1,7 @@
+from Modulos.modulos import *
+
+while True:
+    menu()
+
+    opc = input("Ingrese la opcion de la figura que desea calcular el area \n")
+
