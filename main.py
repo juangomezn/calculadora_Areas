83d102110df0901eea9c1952fb67c81360ac0a68
juangomezn <gomezninoj681@gmail.com>
2024-10-28 20:36:58 -0500

    feat :rocket: Implementacion de opcion para salir del programa y validacion  para opciones del menu

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from Modulos.modulos import *
+from time import sleep
 
 while True:
     menu()
 
     opc = input("Ingrese la opcion de la figura que desea calcular el area \n")
+
+    if opc and not [1, 2, 3, 4, 5]:
+        print("Opcion invalida")
+        print("Ingrese una de las opciones mostradas en el menu")
+        continue
 
     if opc == "1":
         area_Rectangulo()
@@ -16,3 +22,9 @@ while True:
 
     if opc == "4":
         area_Rombo()
+
+    if opc == "0":
+        print("Gracias por utilizar el programa de calculo de areas")
+        print("Saliendo del programa...")
+        sleep(1)
+        
